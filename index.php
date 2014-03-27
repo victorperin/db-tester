47a913aee06136930e2063a5b7e38f2a65342dc5
Victor Perin <victor_perin@nerdhitech.com.br>
2014-03-27 10:39:44 -0300

    Inclusion of an instalation script. This script must be created.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 ﻿<!DOCTYPE html>
-<html lang="br">
+<html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	</head>
@@ -7,18 +7,28 @@
 
 		<?php
 
-	include 'config.php';
-
-
-
-
+			include 'config.php';
+			
 			set_time_limit(0);
 			$db = mysqli_connect($db_server,$db_user,$db_pass,$db_database);
 			
+			
+			$string_step = "select instalation_step from db_tester";
+			$query_step = mysqli_query($db,$string_step);	
+			while($inst_step = mysqli_fetch_array($query_step)){
+				$instalation_step = $row['instalation_step'];
+			}
+			
+			if($instalation_step <> 5 ) {
+				include 'install.php';
+				exit;
+			}
+			
+			
+			
+			
+			
 			echo 'PHP Time: '.date("Y-m-d H:i:s");
-
-
-
 			for ($x=0; $x<=$number_of_queries_by_feature; $x++) {
 				$select_simples = "select  Name from city";
 				$start_time=microtime(true);
