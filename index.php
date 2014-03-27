05dcfc82f15afc2988666c4239f2f72220c1a88f
Victor Perin <victor_perin@nerdhitech.com.br>
2014-03-27 09:49:42 -0300

    Insert include of config.php

--- index.php
+++ index.php
@@ -7,11 +7,7 @@
 
 		<?php
 
-			$db_server = "127.0.0.1";
-			$db_user = "root";
-			$db_pass = "";
-			$db_database = "test";
-			$number_of_queries_by_feature = 500;
+	include 'config.php';
 
 
 
